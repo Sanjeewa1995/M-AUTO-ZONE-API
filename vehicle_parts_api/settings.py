cba56f1408cedf2bd40ab61d4334ef7d401a6f03
Malaka Sanjeewa <malaka@creatit.com.au>
2025-10-25 23:40:33 +0530

    Temporarily disable S3 to test image upload with local storage

--- vehicle_parts_api/settings.py
+++ vehicle_parts_api/settings.py
@@ -232,7 +232,7 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='malakasanjeewa1995@gmail.com')
 
 # AWS S3 Configuration
-USE_S3 = config('USE_S3', default=False, cast=bool)
+USE_S3 = False  # Temporarily disabled to test image upload
 
 if USE_S3:
     # AWS S3 settings
