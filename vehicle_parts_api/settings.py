cb50855273c670d9ce61279dcfb9912a22705f51
Malaka Sanjeewa <malaka@creatit.com.au>
2025-10-26 14:30:00 +0530

    Fix AWS configuration for CI environment - add default values and conditional S3 enablement

--- vehicle_parts_api/settings.py
+++ vehicle_parts_api/settings.py
@@ -232,13 +232,14 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='malakasanjeewa1995@gmail.com')
 
 # AWS S3 Configuration
-USE_S3 = True  # Enable S3 for production
+USE_S3 = config('USE_S3', default=False, cast=bool)
 
-if USE_S3:
+# Only enable S3 if credentials are provided
+if USE_S3 and config('AWS_ACCESS_KEY_ID', default='') and config('AWS_STORAGE_BUCKET_NAME', default=''):
     # AWS S3 settings
-    AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+    AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID', default='')
+    AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY', default='')
+    AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME', default='')
     AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME', default='us-east-1')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
     AWS_DEFAULT_ACL = None
